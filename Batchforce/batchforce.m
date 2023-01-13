0387fdeefebdaff9c9e4f73fd8764b610fd68ed9
AlexWinkel <akw48@cam.ac.uk>
2023-01-13 11:34:43 +0000

    Moved/fixed filtering curves by deflecion/setpoint ratio

--- Batchforce/batchforce.m
+++ Batchforce/batchforce.m
@@ -281,19 +281,6 @@ for i = 1:e
         [time, ~, ~] = FindColumnsNeeded(headerinfo, 'seriesTime', 'measuredHeight'); % JB 13/01/20 line added to include time
         rawdata{vDefl} = smooth(rawdata{vDefl},10); % inserted by David 14/03/13, altered by Julia 23/10/18 to accommodate new variable
         rawdata = CleanData(rawdata, vDefl, mHeight, time); % JB 13/01/20 line amended to include time
-%% Start: section for skipping files if range of Vdefl is less than 70% of setpoint
-        frange = range(cell2mat(rawdata(2))); 
-        spv = GetHeaderValue(headerinfo,'relative-setpoint');  
-        sens = GetHeaderValue(headerinfo,'sensitivity');  
-        springConstant = GetHeaderValue(headerinfo,'springConstant'); 
-        spf = spv*sens*springConstant;
-        acceptable_ratio = 0.7; 
-        if frange/spf < acceptable_ratio  
-            fprintf('Warning - Skipped because the range in force values is less than %i%% of setpoint.\n',round(acceptable_ratio*100));
-            continue      
-        end    
-        clear frange spv sens springConstant acceptable_ratio;
-%% End: section for skipping files if range of Vdefl is less than 70% of setpoint
 
         % log value of variables vDefl and mHeight
         name = fullfile(PathName,'Log_vDefl_mHeight_values.log');
@@ -451,10 +438,6 @@ for i = 1:e
                 fprintf('%s: the indentation was more than R/3, data cropped\n', FileName{1,i});
             end
             filename = fullfile(PathName,[FileName{i}(1:end-4) '.mat']);
-
-            if 1 == exist('RESULTS', 'var')
-                save(filename, 'RESULTS', '-mat')
-            end
             
             rawdata = origrawdata;
             contactpointindex = RESULTS(1,5);
@@ -483,7 +466,26 @@ for i = 1:e
             newapproachdata(:,1) = (-1).*newapproachdata(:,1);
             
             fullcurve = [newapproachdata;indentationdata];
-            
+            %% Start: section for skipping files if range of Vdefl is less than 70% of setpoint
+            frange = range(fullcurve(:,2));        
+            spv = GetHeaderValue(headerinfo,'relative-setpoint');  
+            sens = GetHeaderValue(headerinfo,'sensitivity');  
+            springConstant = GetHeaderValue(headerinfo,'springConstant'); 
+            spf = spv*sens*springConstant;
+            acceptable_ratio = 0.7; 
+            if frange/spf < acceptable_ratio  
+                fprintf('Warning - Skipped because the range in force values is less than %i%% of setpoint.\n',round(acceptable_ratio*100));
+                continue      
+            end
+
+            clear frange spv sens springConstant acceptable_ratio;
+%% End: section for skipping files if range of Vdefl is less than 70% of setpoint
+
+%% Save the mat file
+            if 1 == exist('RESULTS', 'var')
+                save(filename, 'RESULTS', '-mat')
+            end
+
             clear approachdata forcecurvedata numberofdatapoints
             
             %% Plot the corrected curve
